ab13ab7b114285fef3d03246d5673c73dd8acc6f
Jack Mordaunt <jackmordaunt@gmail.com>
2020-11-18 15:47:48 +0800

    feat: init

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// Goal: To free up resources, releases that have not had any recent activity could be deleted without being missed. 
+// Requirements:
+// - Take the number to keep as a parameter
+// - Output the releases that should be kept
+// - Log why a release is kept
+// - Focus on the algorithm rather than a user interface
+namespace ReleaseRetention
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+}
