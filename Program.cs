95e5e289f05e9a56fc53a45712e875fc8296f332
Jack Mordaunt <jackmordaunt@gmail.com>
2020-11-19 20:41:16 +0800

    feat: retain algorithm

--- Program.cs
+++ Program.cs
@@ -1,19 +1,154 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using json = System.Text.Json.JsonSerializer;
 
-// Goal: To free up resources, releases that have not had any recent activity could be deleted without being missed. 
-// Requirements:
-// - Take the number to keep as a parameter
-// - Output the releases that should be kept
-// - Log why a release is kept
-// - Focus on the algorithm rather than a user interface
 namespace ReleaseRetention
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Retainer r = Load();
+            if (args.Length > 0)
+            {
+                string path = args[0];
+                Console.Write("loading json files from {0}\n", Path.GetFullPath(path));
+                r = Load(path);
+            }
+            else
+            {
+                Console.Write("using baked in test data\n");
+            }
+            Console.Write("Releases to retain\n");
+            foreach (var retained in r.Retain(1))
+            {
+                Console.Write("  -> {0}\n", retained);
+            }
+        }
+
+        // Load test data statically. 
+        static Retainer Load()
+        {
+            // TODO: hardcode some test data. 
+            return new Retainer
+            {
+            };
+        }
+
+        // Load test data from disk at the given path. 
+        static Retainer Load(string dir)
+        {
+            return new Retainer
+            {
+                releases = json.Deserialize<List<Release>>(File.ReadAllText(Path.Join(dir, "Releases.json"))),
+                projects = json.Deserialize<List<Project>>(File.ReadAllText(Path.Join(dir, "Projects.json"))),
+                environments = json.Deserialize<List<Environment>>(File.ReadAllText(Path.Join(dir, "Environments.json"))),
+                deployments = json.Deserialize<List<Deployment>>(File.ReadAllText(Path.Join(dir, "Deployments.json"))),
+            };
         }
     }
 
+    // Retainer implements logic for retaining releases. 
+    class Retainer
+    {
+        public List<Project> projects;
+        public List<Release> releases;
+        public List<Environment> environments;
+        public List<Deployment> deployments;
+
+        // Retain the most recent n releases per project. 
+        // Deployed releases are retained regardless.
+        // Orphaned releases are pruned regardless, unless deployed.
+        public List<RetainedRelease> Retain(int n)
+        {
+            var retained = new List<RetainedRelease>();
+            foreach (Project p in this.projects)
+            {
+                // Grab the releases for this project ordered by most recent first. 
+                var releases = from r in this.releases
+                               where r.ProjectId == p.Id
+                               orderby r.Created descending
+                               select r;
+                // Record up to n releases to be retained. 
+                // Releases that are deployed are retained by default. 
+                for (int ii = 0; ii < releases.Count(); ii++)
+                {
+                    Release r = releases.ElementAt(ii);
+                    if (ii < n)
+                    {
+                        retained.Add(new RetainedRelease
+                        {
+                            project = p,
+                            release = r,
+                            reason = String.Format("recency {0}/{1}", ii + 1, n),
+                        });
+                    }
+                    else
+                    {
+                        bool isDeployed = this.deployments.Any((Deployment d) => d.ReleaseId == r.Id);
+                        if (isDeployed)
+                        {
+                            Deployment d = this.deployments.Where((Deployment d) => d.ReleaseId == r.Id).First();
+                            Environment env = this.environments.Find((Environment env) => d.EnvironmentId == env.Id);
+                            retained.Add(new RetainedRelease
+                            {
+                                project = p,
+                                release = r,
+                                reason = String.Format("currently deployed to {0}", env.Name),
+                            });
+                        }
+                    }
+                }
+            }
+            return retained;
+        }
+    }
+
+    // RetainedRelease records a release to be retained and why. 
+    // Note: reason could be an enum if we wanted to programatically react to it.
+    struct RetainedRelease
+    {
+        public Project project { get; set; }
+        public Release release { get; set; }
+        public string reason { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0}: {1} ({2}): {3}",
+                this.project.Name,
+                this.release.Id,
+                this.release.Version != null ? "v" + this.release.Version : "unversioned",
+                this.reason);
+        }
+    }
+
+    class Release
+    {
+        public string Id { get; set; }
+        public string ProjectId { get; set; }
+        public string Version { get; set; }
+        public DateTime Created { get; set; }
+    }
+
+    class Deployment
+    {
+        public string Id { get; set; }
+        public string ReleaseId { get; set; }
+        public string EnvironmentId { get; set; }
+        public string DeployedAt { get; set; }
+    }
+
+    class Environment
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    class Project
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
 }
