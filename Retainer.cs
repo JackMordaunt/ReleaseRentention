0968529e838a94fad8bc70d12dbabf4da2b2074f
Jack Mordaunt <jackmordaunt@gmail.com>
2020-11-25 17:22:54 +0800

    feat: implement retain by project-environment

--- Retainer.cs
+++ Retainer.cs
@@ -9,10 +9,10 @@ namespace ReleaseRetention
     // Retainer implements logic for retaining releases. 
     class Retainer
     {
-        public List<Project> projects;
-        public List<Release> releases;
-        public List<Environment> environments;
-        public List<Deployment> deployments;
+        public List<Project> Projects = new List<Project>();
+        public List<Release> Releases = new List<Release>();
+        public List<Environment> Environments = new List<Environment>();
+        public List<Deployment> Deployments = new List<Deployment>();
 
         // Retain the most recent n releases per project. 
         // Deployed releases are retained regardless.
@@ -22,12 +22,12 @@ namespace ReleaseRetention
             var retained = new List<RetainedRelease>();
             // Note: Group by project id lets us handle the case where a release 
             // is deployed but the project has been deleted. 
-            var groups = this.releases.GroupBy(
+            var groups = this.Releases.GroupBy(
                 r => r.ProjectId,
                 r => r,
                 (projectId, group) => new
                 {
-                    Project = this.projects.Where((Project p) => p.Id == projectId).FirstOrDefault(),
+                    Project = this.Projects.Where((Project p) => p.Id == projectId).FirstOrDefault(),
                     Releases = group.OrderByDescending((Release r) => r.Created),
                 });
             foreach (var g in groups)
@@ -40,22 +40,61 @@ namespace ReleaseRetention
                     {
                         retained.Add(new RetainedRelease
                         {
-                            project = g.Project,
-                            release = r,
-                            reason = String.Format("recency {0}/{1}", ii + 1, n),
+                            Project = g.Project,
+                            Release = r,
+                            Reason = String.Format("recency {0}/{1}", ii + 1, n),
                         });
                     }
                     else if (this.IsDeployed(r.Id))
                     {
-                        Deployment d = this.deployments.Find((Deployment d) => d.ReleaseId == r.Id);
-                        Environment env = this.environments.Find((Environment env) => d.EnvironmentId == env.Id);
+                        Deployment d = this.Deployments.Find((Deployment d) => d.ReleaseId == r.Id);
+                        Environment env = this.Environments.Find((Environment env) => d.EnvironmentId == env.Id);
                         retained.Add(new RetainedRelease
                         {
-                            project = g.Project,
-                            release = r,
-                            reason = String.Format("currently deployed to {0}", env.Name),
+                            Project = g.Project,
+                            Release = r,
+                            Reason = String.Format("currently deployed to {0}", env.Name),
                         });
                     }
+                }
+            }
+            return retained;
+        }
+
+        // RetainDeploymet retains the most recent `n` deployments for a given 
+        // project-environment pair. 
+        public List<RetainedDeployment> RetainDeployments(int n)
+        {
+            List<RetainedDeployment> retained = new List<RetainedDeployment>();
+            // Iterate deployment-wise and pull all the data together.
+            // Take up to n number deployments ordered by most recent first.
+            var groups = this.Deployments.GroupBy(
+                d => new
+                {
+                    Project = this.Projects.Find(p => p.Id == this.Releases.Find(r => r.Id == d.ReleaseId).ProjectId).Id,
+                    Environment = this.Environments.Find(e => e.Id == d.EnvironmentId).Id,
+                },
+                d => new
+                {
+                    Deployment = d,
+                    Project = this.Projects.Find(p => p.Id == this.Releases.Find(r => r.Id == d.ReleaseId).ProjectId),
+                    Environment = this.Environments.Find(e => e.Id == d.EnvironmentId),
+                },
+                (d, group) => group.OrderByDescending(d => d.Deployment.DeployedAt).Take(n)
+            );
+            // Flatten data into list. 
+            foreach (var group in groups)
+            {
+                foreach (var d in group)
+                {
+                    var retain = new RetainedDeployment
+                    {
+                        DeploymentId = d.Deployment.Id,
+                        ProjectId = d.Project.Id,
+                        EnvironmentId = d.Environment.Id,
+                        ReleaseId = d.Deployment.ReleaseId,
+                    };
+                    retained.Add(retain);
                 }
             }
             return retained;
@@ -65,19 +104,19 @@ namespace ReleaseRetention
         // An orphaned release is a release with no active project.
         public IEnumerable<Release> Orphans()
         {
-            return this.releases.Where(r => !this.projects.Any(p => p.Id == r.ProjectId));
+            return this.Releases.Where(r => !this.Projects.Any(p => p.Id == r.ProjectId));
         }
 
         // Deployed returns a list of deployed releases.
         public IEnumerable<Release> Deployed()
         {
-            return this.releases.Where(r => this.deployments.Any(d => d.ReleaseId == r.Id));
+            return this.Releases.Where(r => this.Deployments.Any(d => d.ReleaseId == r.Id));
         }
 
         // IsDeployed if the given release has an associated deployment. 
         public bool IsDeployed(string ReleaseId)
         {
-            return this.deployments.Any((Deployment d) => d.ReleaseId == ReleaseId);
+            return this.Deployments.Any((Deployment d) => d.ReleaseId == ReleaseId);
         }
 
         // Load test data from disk at the given path. 
@@ -85,10 +124,10 @@ namespace ReleaseRetention
         {
             return new Retainer
             {
-                releases = json.Deserialize<List<Release>>(File.ReadAllText(Path.Join(dir, "Releases.json"))),
-                projects = json.Deserialize<List<Project>>(File.ReadAllText(Path.Join(dir, "Projects.json"))),
-                environments = json.Deserialize<List<Environment>>(File.ReadAllText(Path.Join(dir, "Environments.json"))),
-                deployments = json.Deserialize<List<Deployment>>(File.ReadAllText(Path.Join(dir, "Deployments.json"))),
+                Releases = json.Deserialize<List<Release>>(File.ReadAllText(Path.Join(dir, "Releases.json"))),
+                Projects = json.Deserialize<List<Project>>(File.ReadAllText(Path.Join(dir, "Projects.json"))),
+                Environments = json.Deserialize<List<Environment>>(File.ReadAllText(Path.Join(dir, "Environments.json"))),
+                Deployments = json.Deserialize<List<Deployment>>(File.ReadAllText(Path.Join(dir, "Deployments.json"))),
             };
         }
     }
@@ -97,17 +136,37 @@ namespace ReleaseRetention
     // Note: reason could be an enum if we wanted to programatically react to it.
     struct RetainedRelease
     {
-        public Project project { get; set; }
-        public Release release { get; set; }
-        public string reason { get; set; }
+        public Project Project { get; set; }
+        public Release Release { get; set; }
+        public string Reason { get; set; }
 
         public override string ToString()
         {
             return String.Format("{0}: {1} ({2}): {3}",
-                this.project.Name != null ? this.project.Name : this.release.ProjectId,
-                this.release.Id,
-                this.release.Version != null ? "v" + this.release.Version : "unversioned",
-                this.reason);
+                this.Project.Name != null ? this.Project.Name : this.Release.ProjectId,
+                this.Release.Id,
+                this.Release.Version != null ? "v" + this.Release.Version : "unversioned",
+                this.Reason);
+        }
+    }
+
+    // RetaiendDeploymet records a deployment to be retained and why. 
+    struct RetainedDeployment
+    {
+        public string ReleaseId { get; set; }
+        public string ProjectId { get; set; }
+        public string EnvironmentId { get; set; }
+        public string DeploymentId { get; set; }
+        // public string Reason { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format(
+                "(Deployment = {0}, Project = {1}, Environment = {2}, Release = {3})",
+                this.DeploymentId,
+                this.ProjectId,
+                this.EnvironmentId,
+                this.ReleaseId);
         }
     }
 
@@ -124,7 +183,7 @@ namespace ReleaseRetention
         public string Id { get; set; }
         public string ReleaseId { get; set; }
         public string EnvironmentId { get; set; }
-        public string DeployedAt { get; set; }
+        public DateTime DeployedAt { get; set; }
     }
 
     struct Environment
@@ -138,4 +197,5 @@ namespace ReleaseRetention
         public string Id { get; set; }
         public string Name { get; set; }
     }
+
 }
--- Test.cs
+++ Test.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 using System;
+using System.Collections.Generic;
 
 namespace ReleaseRetention
 {
@@ -21,11 +22,11 @@ namespace ReleaseRetention
             // Therefore retained.Count = releases.Count - undeployedOrphans.Count
 
             var undeployedOrphans = r.Orphans()
-                .Where(o => !r.deployments.Any(d => d.ReleaseId == o.Id));
+                .Where(o => !r.Deployments.Any(d => d.ReleaseId == o.Id));
 
             Assert.AreEqual(
-                r.Retain(r.releases.Count()).Count(),
-                r.releases.Count() - undeployedOrphans.Count()
+                r.Retain(r.Releases.Count()).Count(),
+                r.Releases.Count() - undeployedOrphans.Count()
             );
         }
 
@@ -44,14 +45,342 @@ namespace ReleaseRetention
         {
             // Orphaned releases that are not deployed are not retained. 
 
-            var retainedOrphans = r.Retain(r.releases.Count())
-                .Where(retained => r.Orphans().Contains(retained.release));
+            var retainedOrphans = r.Retain(r.Releases.Count())
+                .Where(retained => r.Orphans().Contains(retained.Release));
 
             var deployedOrphans = r.Orphans()
-                .Where(o => r.deployments.Any(d => d.ReleaseId == o.Id));
+                .Where(o => r.Deployments.Any(d => d.ReleaseId == o.Id));
 
             Assert.AreEqual(deployedOrphans.Count(), retainedOrphans.Count());
-            Assert.IsFalse(retainedOrphans.Any(o => !r.deployments.Any(d => d.ReleaseId == o.release.Id)));
+            Assert.IsFalse(retainedOrphans.Any(o => !r.Deployments.Any(d => d.ReleaseId == o.Release.Id)));
+        }
+
+        [TestMethod]
+        public void TestRetainDeployments()
+        {
+            var tests = new[] {
+                new {
+                    Label = "empty data should return an empty list",
+                    N = 0,
+                    Data = new Retainer {},
+                    Want = new List<RetainedDeployment>{}
+                },
+                new {
+                    Label = "empty data should return an empty list, regardless of n",
+                    N = 100,
+                    Data = new Retainer {},
+                    Want = new List<RetainedDeployment>{}
+                },
+                new {
+                    Label = "should retain no deployments with n of zero",
+                    N = 0,
+                    Data = new Retainer {
+                        Projects = new List<Project>(){
+                            new Project {
+                                Id = "project-1",
+                                Name = "project-1",
+                            },
+                            new Project {
+                                Id = "project-2",
+                                Name = "project-2",
+                            },
+                        },
+                        Releases = new List<Release>(){
+                            new Release {
+                                Id = "relase-1",
+                                ProjectId = "project-1",
+                                Version = "",
+                                Created = new DateTime(),
+                            },
+                        },
+                        Deployments = new List<Deployment>(){
+                            new Deployment {
+                                Id = "deployment-1",
+                                ReleaseId = "release-1",
+                                EnvironmentId = "staging",
+                                DeployedAt = new DateTime(),
+                            },
+                        },
+                        Environments = new List<Environment>(){
+                            new Environment{
+                                Id = "staging",
+                                Name = "staging",
+                            }
+                        },
+                    },
+                    Want = new List<RetainedDeployment>{}
+                },
+                new {
+                    Label = "should retain the single most recent deployment per project-environment",
+                    N = 1,
+                    Data = new Retainer {
+                        Projects = new List<Project>(){
+                            new Project {
+                                Id = "project-1",
+                            },
+                        },
+                        Releases = new List<Release>(){
+                            new Release {
+                                Id = "release-1",
+                                ProjectId = "project-1",
+                                Version = "",
+                                Created = new DateTime(),
+                            },
+                        },
+                        Deployments = new List<Deployment>(){
+                            new Deployment {
+                                Id = "deployment-1",
+                                ReleaseId = "release-1",
+                                EnvironmentId = "staging",
+                                DeployedAt = new DateTime(),
+                            },
+                        },
+                        Environments = new List<Environment>(){
+                            new Environment{
+                                Id = "staging",
+                            }
+                        },
+                    },
+                    Want = new List<RetainedDeployment>(){
+                        new RetainedDeployment{
+                            DeploymentId = "deployment-1",
+                            ProjectId = "project-1",
+                            EnvironmentId = "staging",
+                            ReleaseId = "release-1"
+                        },
+                    }
+                },
+                new {
+                    Label = "should retain the single most recent deployment per project-environment",
+                    N = 1,
+                    Data = new Retainer {
+                        Projects = new List<Project>(){
+                            new Project {
+                                Id = "project-1",
+                            },
+                            new Project {
+                                Id = "project-2",
+                            },
+                        },
+                        Releases = new List<Release>(){
+                            new Release {
+                                Id = "release-1",
+                                ProjectId = "project-1",
+                                Version = "",
+                                Created = new DateTime(),
+                            },
+                            new Release {
+                                Id = "release-2",
+                                ProjectId = "project-1",
+                                Version = "",
+                                Created = new DateTime(),
+                            },
+                            new Release {
+                                Id = "release-3",
+                                ProjectId = "project-2",
+                                Version = "",
+                                Created = new DateTime(),
+                            },
+                            new Release {
+                                Id = "release-4",
+                                ProjectId = "project-2",
+                                Version = "",
+                                Created = new DateTime(),
+                            },
+                        },
+                        Deployments = new List<Deployment>(){
+                            new Deployment {
+                                Id = "deployment-1",
+                                ReleaseId = "release-1",
+                                EnvironmentId = "staging",
+                                DeployedAt = new DateTime(),
+                            },
+                            new Deployment {
+                                Id = "deployment-2",
+                                ReleaseId = "release-2",
+                                EnvironmentId = "production",
+                                DeployedAt = new DateTime(),
+                            },
+                            new Deployment {
+                                Id = "deployment-3",
+                                ReleaseId = "release-3",
+                                EnvironmentId = "staging",
+                                DeployedAt = new DateTime(),
+                            },
+                            new Deployment {
+                                Id = "deployment-4",
+                                ReleaseId = "release-4",
+                                EnvironmentId = "production",
+                                DeployedAt = DateTime.MinValue,
+                            },
+                            new Deployment {
+                                Id = "deployment-5",
+                                ReleaseId = "release-4",
+                                EnvironmentId = "production",
+                                DeployedAt = DateTime.MaxValue,
+                            },
+                        },
+                        Environments = new List<Environment>(){
+                            new Environment{
+                                Id = "staging",
+                            },
+                            new Environment{
+                                Id = "production",
+                            }
+                        },
+                    },
+                    Want = new List<RetainedDeployment>(){
+                        new RetainedDeployment{
+                            ProjectId = "project-1",
+                            ReleaseId = "release-1",
+                            DeploymentId = "deployment-1",
+                            EnvironmentId = "staging",
+                        },
+                        new RetainedDeployment{
+                            ProjectId = "project-1",
+                            ReleaseId = "release-2",
+                            DeploymentId = "deployment-2",
+                            EnvironmentId = "production",
+                        },
+                        new RetainedDeployment{
+                            ProjectId = "project-2",
+                            ReleaseId = "release-3",
+                            DeploymentId = "deployment-3",
+                            EnvironmentId = "staging",
+                        },
+                        new RetainedDeployment{
+                            ProjectId = "project-2",
+                            ReleaseId = "release-4",
+                            DeploymentId = "deployment-5",
+                            EnvironmentId = "production",
+                        },
+                    }
+                },
+                new {
+                    Label = "retain up to two most recent deployments per project-environment",
+                    N = 2,
+                    Data = new Retainer {
+                        Projects = new List<Project>(){
+                            new Project {
+                                Id = "project-1",
+                            },
+                            new Project {
+                                Id = "project-2",
+                            },
+                        },
+                        Releases = new List<Release>(){
+                            new Release {
+                                Id = "release-1",
+                                ProjectId = "project-1",
+                                Version = "",
+                                Created = new DateTime(),
+                            },
+                            new Release {
+                                Id = "release-2",
+                                ProjectId = "project-1",
+                                Version = "",
+                                Created = new DateTime(),
+                            },
+                            new Release {
+                                Id = "release-3",
+                                ProjectId = "project-2",
+                                Version = "",
+                                Created = new DateTime(),
+                            },
+                            new Release {
+                                Id = "release-4",
+                                ProjectId = "project-2",
+                                Version = "",
+                                Created = new DateTime(),
+                            },
+                        },
+                        Deployments = new List<Deployment>(){
+                            new Deployment {
+                                Id = "deployment-1",
+                                ReleaseId = "release-1",
+                                EnvironmentId = "staging",
+                                DeployedAt = new DateTime(),
+                            },
+                            new Deployment {
+                                Id = "deployment-2",
+                                ReleaseId = "release-2",
+                                EnvironmentId = "production",
+                                DeployedAt = new DateTime(),
+                            },
+                            new Deployment {
+                                Id = "deployment-3",
+                                ReleaseId = "release-3",
+                                EnvironmentId = "staging",
+                                DeployedAt = new DateTime(),
+                            },
+                            new Deployment {
+                                Id = "deployment-4",
+                                ReleaseId = "release-4",
+                                EnvironmentId = "production",
+                                DeployedAt = DateTime.MinValue,
+                            },
+                            new Deployment {
+                                Id = "deployment-5",
+                                ReleaseId = "release-4",
+                                EnvironmentId = "production",
+                                DeployedAt = DateTime.MaxValue,
+                            },
+                        },
+                        Environments = new List<Environment>(){
+                            new Environment{
+                                Id = "staging",
+                            },
+                            new Environment{
+                                Id = "production",
+                            }
+                        },
+                    },
+                    Want = new List<RetainedDeployment>(){
+                        new RetainedDeployment{
+                            ProjectId = "project-1",
+                            ReleaseId = "release-1",
+                            DeploymentId = "deployment-1",
+                            EnvironmentId = "staging",
+                        },
+                        new RetainedDeployment{
+                            ProjectId = "project-1",
+                            ReleaseId = "release-2",
+                            DeploymentId = "deployment-2",
+                            EnvironmentId = "production",
+                        },
+                        new RetainedDeployment{
+                            ProjectId = "project-2",
+                            ReleaseId = "release-3",
+                            DeploymentId = "deployment-3",
+                            EnvironmentId = "staging",
+                        },
+                        new RetainedDeployment{
+                            ProjectId = "project-2",
+                            ReleaseId = "release-4",
+                            DeploymentId = "deployment-5",
+                            EnvironmentId = "production",
+                        },
+                        new RetainedDeployment{
+                            ProjectId = "project-2",
+                            ReleaseId = "release-4",
+                            DeploymentId = "deployment-4",
+                            EnvironmentId = "production",
+                        },
+                    }
+                },
+            }.ToList();
+            foreach (var test in tests)
+            {
+                var got = test.Data.RetainDeployments(test.N);
+                Assert.IsTrue(
+                    Enumerable.SequenceEqual(got, test.Want),
+                    String.Format(
+                        "{0}: \nwant \n\t{1}, \ngot \n\t{2}",
+                        test.Label,
+                        string.Join(",\n\t", test.Want),
+                        string.Join(",\n\t", got)
+                    ));
+            }
         }
     }
 }
